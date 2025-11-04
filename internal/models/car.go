a116f472cf3de15e16d11f9a16a8298dd955ce7d
MitrKor <mitrofankorshunov@gmail.com>
2025-11-04 14:11:20 +0300

    Модель

--- internal/models/car.go
+++ internal/models/car.go
@@ -1,1 +1,11 @@
 package models
+
+type Car struct {
+	ID         int      `json:"id" db:"id"`
+	Mark       string   `json:"mark" db:"mark"`
+	Model      string   `json:"model" db:"model"`
+	OwnerCount int      `json:"owner_count" db:"owner_count"`
+	Price      int      `json:"price" db:"price"`
+	Currency   string   `json:"currency" db:"currency"`
+	Options    []string `json:"options" db:"options"`
+}
