213c70b01581e577fa3467c196d831399b4173f2
MitrKor <mitrofankorshunov@gmail.com>
2025-11-04 18:52:05 +0300

    обработчик Create + зависимости

--- internal/handler/car_handler.go
+++ internal/handler/car_handler.go
@@ -1,1 +1,31 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/MitrofanK/Test-Menti/internal/models"
+	"github.com/MitrofanK/Test-Menti/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type CarHandler struct {
+	service service.CarService
+}
+
+func NewCarHandler(s service.CarService) *CarHandler {
+	return &CarHandler{service: s}
+}
+
+func (h *CarHandler) Create(c *gin.Context) {
+	var input models.Car
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	id, err := h.service.Create(c.Request.Context(), input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"id": id})
+}
